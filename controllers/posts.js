df15b7f856cbe740ef36d464dc2a4b0ca972afff
Jessica Sarkar <jessicalosee@gmail.com>
2022-11-27 15:22:29 -0600

    removed unused code

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,4 @@
 const cloudinary = require("../middleware/cloudinary");
-const Post = require("../models/Post");
 const Streak = require("../models/Streak")
 const Journal = require("../models/Journal")
 const streakControllers = require("../controllers/streak")
@@ -38,7 +37,6 @@ function benefitDay (day) {
 module.exports = {
   getProfile: async (req, res) => {
     try {
-      const posts = await Post.find({ user: req.user.id });
       const userStreak = await Streak.find({ userId: req.user.id });
       const journalEntries = await Journal.find({ userId: req.user.id })
 
@@ -76,7 +74,7 @@ module.exports = {
         }
       })//trigger count mess ends here if you want to just delete this garbage
 
-      res.render("profile.ejs", { posts: posts, user: req.user, message: todayMsg, streak: streak, journal: journalEntries });
+      res.render("profile.ejs", { user: req.user, message: todayMsg, streak: streak, journal: journalEntries });
     } catch (err) {
       console.log(err);
     }
@@ -89,22 +87,8 @@ module.exports = {
     }
     
   },
-  getFeed: async (req, res) => {
-    try {
-      const posts = await Post.find().sort({ createdAt: "desc" }).lean();
-      res.render("feed.ejs", { posts: posts });
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  getPost: async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id);
-      res.render("post.ejs", { post: post, user: req.user });
-    } catch (err) {
-      console.log(err);
-    }
-  },
+ 
+
   createJournal: async (req, res) => {
     //might be able to add logic that only allows user to submit this form once/day
     try {
@@ -147,51 +131,6 @@ module.exports = {
       console.log(err);
     }
   },
-  createPost: async (req, res) => {
-    try {
-      // Upload image to cloudinary
-      const result = await cloudinary.uploader.upload(req.file.path);
-
-      await Post.create({
-        title: req.body.title,
-        image: result.secure_url,
-        cloudinaryId: result.public_id,
-        caption: req.body.caption,
-        likes: 0,
-        user: req.user.id,
-      });
-      console.log("Post has been added!");
-      res.redirect("/profile");
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  likePost: async (req, res) => {
-    try {
-      await Post.findOneAndUpdate(
-        { _id: req.params.id },
-        {
-          $inc: { likes: 1 },
-        }
-      );
-      console.log("Likes +1");
-      res.redirect(`/post/${req.params.id}`);
-    } catch (err) {
-      console.log(err);
-    }
-  },
-  deletePost: async (req, res) => {
-    try {
-      // Find post by id
-      let post = await Post.findById({ _id: req.params.id });
-      // Delete image from cloudinary
-      await cloudinary.uploader.destroy(post.cloudinaryId);
-      // Delete post from db
-      await Post.remove({ _id: req.params.id });
-      console.log("Deleted Post");
-      res.redirect("/profile");
-    } catch (err) {
-      res.redirect("/profile");
-    }
-  },  
+  
+   
 };
